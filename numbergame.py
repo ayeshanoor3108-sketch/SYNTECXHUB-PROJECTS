99cb2d6fc41d38b5ef5e07dcee343a3715944eac
Noor Ayesha <ayeshanoor3108@gmail.com>
2026-06-24 08:26:27 +0530

    Add files via upload

---
+++ numbergame.py
@@ -0,0 +1,155 @@
+import random 
+
+best_score = None
+best_player = ""
+total_score = 0
+
+
+print("\n🎮✨ WELCOME TO NUMBER GUESSING CHALLENGE ✨🎮")
+print("-"*45)
+print("🧠 Test your guessing skills!")
+print("🎯 Guess the hidden number and make a new record!")
+print("🏆 Earn points, beat records and become a legend!")
+print("-"*45)
+
+
+ready = input("\n🚀 Are you ready to start? (y/n): ").lower()
+
+
+if ready != "y":
+    print("👋 No worries! Come back when you are ready.")
+    exit()
+
+
+print("\n🔥 Great! Let's begin")
+
+
+print("\n🎮 Select Mode")
+print("1. Solo Mode")
+print("2. Friends Mode")
+  
+
+mode = input("choose mode (1/2): ")
+while mode not in ["1","2"]:
+     print("Invalid Mode!")
+     mode = input("choose mode (1/2): ")
+
+
+while True:
+     print("\n===Number Guessing Game ===")
+     print("1. Easy (1-50)")
+     print("2. Medium (1-100)")
+     print("3. Hard (1-500)")
+
+     choice = input("choose difficulty (1/2/3): ")
+
+     if choice == "1":
+          limit = 50
+          reward = 10
+
+     elif choice =="2":
+          limit = 100  
+          reward = 20
+
+     elif choice == "3":
+          limit = 500
+          reward = 50
+     else:
+          print("Invalid Choice!")
+          continue
+     number = random.randint(1, limit)
+     attempts = 0
+
+     print(f"\nGuess a number between 1 and {limit}")
+
+     while True:
+          try: 
+               guess = int(input("\tEnter your guess: "))
+               attempts += 1
+
+
+               if attempts == 3:
+                   divisor_found  = False   
+
+                   
+
+                   for i in range(2,11):
+                       if number % i == 0:
+                           print(f"\n💡 BONUS HINT: This number comes in the table of {i}")
+                           divisor_found = True
+                           break 
+                       
+                   if not divisor_found:
+                        print("\n💡 BONUS HINT: This number is a Prime Number")
+
+               if guess == number:
+                    print(f"\nCorrect! You guessed it in {attempts} attempts.")
+                    total_score += reward
+                    print(f"💰 +{reward} Score Added!")
+                    print(f"⭐ Total Score: {total_score}")
+                    if best_score is None:
+                         
+                       if mode == "2":
+                          print("\n🔥 Hey Genius! You are our first best scorer! 🔥")
+
+                          best_player = input("💫What's your lucky name? ")
+
+                          print("\n😊 Okayy got it!")
+                          
+                       else:
+                          print("\n🎉 First personal Record Created!")
+                    
+                       best_score = attempts
+                     
+                
+                    elif attempts < best_score:
+                    
+                        if mode =="2":
+                         
+
+                           print(f"\n🏆 Congrats! You beat the last top scorer {best_player}!")
+                           best_player = input("🤩 Can I know your name Buddy?")
+
+                           print("\n😊 Okay got it!")
+                               
+                        else:
+
+                          print("\n🚀 Amazing!")
+                          print("🔥 You beat your previous record!") 
+                          print(f"🏅 Old Record: {best_score} attempts")
+                          print(f"⭐ New Record: {attempts} attempts")
+
+                        best_score = attempts   
+                    
+                    break
+               elif guess < number:
+                    print("Higher number please!")
+               else:
+                    print("Lower number please!")
+
+               # Motivation Hint after 5 attempts
+               if attempts == 5:
+
+                   print("\n🔥 Hey dude! You are not too far!")
+
+                   if guess < number:
+                       print("⬆️ Try a higher number!")
+                   else:
+                       print("⬇️ Try a lower number!")     
+
+          except ValueError:
+               print("Please enter a valid number.")
+
+     replay = input("\nPlay Again? (y/n): ").lower()
+
+     if replay =="y":
+          continue
+
+     elif replay == "n":
+          print("Thanks for playing!")
+          break 
+
+     else:
+         print("❌ Incorrect choice! Please enter y or n")
+
+                                     
